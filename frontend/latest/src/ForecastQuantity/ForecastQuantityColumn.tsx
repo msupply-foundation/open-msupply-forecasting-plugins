a2b41191bac4d60ad02ba3cbf782e68366032832
James Brunskill <james@sussol.net>
2025-11-07 17:04:29 +1300

    Change to french format...

--- frontend/latest/src/ForecastQuantity/ForecastQuantityColumn.tsx
+++ frontend/latest/src/ForecastQuantity/ForecastQuantityColumn.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   ArrayElement,
   BasicCellLayout,
@@ -8,39 +8,39 @@ import {
   Plugins,
   QueryClientProviderProxy,
   UNDEFINED_STRING_VALUE,
-} from '@openmsupply-client/common';
-import { RequestLineFragment } from '@openmsupply-client/system';
-import { usePluginData } from '../../../../../api';
+} from "@openmsupply-client/common";
+import { RequestLineFragment } from "@openmsupply-client/system";
+import { usePluginData } from "../../../../../api";
 
 const useColumnStore = create<PluginDataStore<RequestLineFragment, string>>(
   (set, get) => ({
     data: [],
-    set: data => set(state => ({ ...state, data })),
-    getById: row =>
+    set: (data) => set((state) => ({ ...state, data })),
+    getById: (row) =>
       get().data.find(({ relatedRecordId }) => relatedRecordId == row.id),
   })
 );
 
 type ForecastQuantityColumn = NonNullable<
-  ArrayElement<Plugins['requestRequisitionLine']>
+  ArrayElement<Plugins["requestRequisitionLine"]>
 >;
 
 export const StateLoader: ArrayElement<
-  ForecastQuantityColumn['tableStateLoader']
-> = props => {
+  ForecastQuantityColumn["tableStateLoader"]
+> = (props) => {
   const { set } = useColumnStore();
 
   const {
     query: { data },
   } = usePluginData({
-    pluginCode: 'forecasting_plugins',
+    pluginCode: "forecasting_plugins",
     filter: {
-      dataIdentifier: { equalTo: 'FORECAST_QUANTITY_INFO' },
+      dataIdentifier: { equalTo: "FORECAST_QUANTITY_INFO" },
       relatedRecordId: { equalAny: props.requestLines.map(({ id }) => id) },
     },
     // By using the line IDs as the keys, it will cause the query to re-fetch
     // whenever a line is added/removed
-    queryKey: props.requestLines.map(line => line.id),
+    queryKey: props.requestLines.map((line) => line.id),
   });
 
   useEffect(() => {
@@ -55,7 +55,7 @@ export const StateLoader: ArrayElement<
 const ForecastColumn = ({ row: rowData }: { row: RequestLineFragment }) => {
   const { getById } = useColumnStore();
 
-  const parsed = JSON.parse(getById(rowData)?.data || '{}');
+  const parsed = JSON.parse(getById(rowData)?.data || "{}");
   const value = parsed?.forecastTotalUnits
     ? String(Math.ceil(parsed?.forecastTotalUnits))
     : UNDEFINED_STRING_VALUE;
@@ -69,10 +69,10 @@ export const ForecastQuantityColumn: ColumnDef<RequestLineFragment> = {
       <ForecastColumn row={row.original} />
     </QueryClientProviderProxy>
   ),
-  id: 'forecast-quantity',
-  header: 'Target stock (population) ', // plugin.forecasting.forecast-amount (can't translate in plugins yet...)
+  id: "forecast-quantity",
+  header: "Niveau de stock cible en unités (prévision démographique)", // plugin.forecasting.forecast-amount (can't translate in plugins yet...)
   description:
-    'The target stock level for this item, calculated using the population served by this store', // 'plugin.forecasting.forecast-amount-description',
+    "Le niveau de stock cible calculé selon les prévisions démographiques", // 'plugin.forecasting.forecast-amount-description',
   enableSorting: false,
   columnIndex: 10,
   defaultHideOnMobile: true,
